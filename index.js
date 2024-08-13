5f2f05ba6e44b4afe4b3cc2159fbcb398c450570
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2024-08-13 14:52:11 -0300

    Adicionei mensagens nas rotas

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ app.get('/Diego',function(req,res){
     res.send("Diego!");
 })
 
+app.get('/:x',function(req,res){
+    const mensagem = "Oi "+req.params.x;
+    res.send(mensagem)
+})
+
+app.get('/:nome/:sobrenome',function(req,res){
+    const mensagem = "Oi "+req.params.nome+" "+req.params.sobrenome;
+    res.send(mensagem)
+})
+
 app.listen(3000);
